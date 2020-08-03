6ce960221a8ffcd2fee8d9afa2a604fcea8bc3c7
Hanwen Wang <allenwang@buaa.edu.cn>
2020-08-03 14:49:52 +0800

    update

--- preparehistojobs.py
+++ preparehistojobs.py
@@ -5,30 +5,30 @@ import os
 import yaml
 
 def prepare_job_script():
-		plotDirectory = jobsDirectory +"/"+year
-		if not os.path.exists(plotDirectory):
+    plotDirectory = jobsDirectory +"/"+year
+    if not os.path.exists(plotDirectory):
       os.makedirs(plotDirectory)
     if not os.path.exists(jobsDirectory +"/jobs"):
       os.makedirs(jobsDirectory +"/jobs")
-		os.system("cp "+baseDir+"/bin/example_ntuple " + jobsDirectory)
-		os.system("cp "+baseDir+"/env.sh " + jobsDirectory)
-		subDirectory = jobsDirectory +"/"+year+"/histograms"
-		if not os.path.exists(subDirectory):
-			os.makedirs(subDirectory)
-		for fileName in samples:
-			scriptFile = open(jobsDirectory+'/jobs/'+'mkhis_'+year+fileName+'.sh','w')
-			scriptLines = ''
-			scriptLines += ('export INITDIR='+jobsDirectory+'\n')
-			scriptLines += ('cd $INITDIR\n')
-			scriptLines += '. ./env.sh ;\n'
-			scriptLines += ("date;\n")
-			scriptLines += ("./example_ntuple input="+baseDir+"/samples/"+year+fileName+".root"+" config="+baseDir+"/config/"+aFile+" output="+subDirectory+"/"+fileName+".root;\n")
+    os.system("cp "+baseDir+"/bin/example_ntuple " + jobsDirectory)
+    os.system("cp "+baseDir+"/env.sh " + jobsDirectory)
+    subDirectory = jobsDirectory +"/"+year+"/histograms"
+    if not os.path.exists(subDirectory):
+      os.makedirs(subDirectory)
+    for fileName in samples:
+      scriptFile = open(jobsDirectory+'/jobs/'+'mkhis_'+year+fileName+'.sh','w')
+      scriptLines = ''
+      scriptLines += ('export INITDIR='+jobsDirectory+'\n')
+      scriptLines += ('cd $INITDIR\n')
+      scriptLines += '. ./env.sh ;\n'
+      scriptLines += ("date;\n")
+      scriptLines += ("./example_ntuple input="+baseDir+"/samples/"+year+fileName+".root"+" config="+baseDir+"/config/"+aFile+" output="+subDirectory+"/"+fileName+".root;\n")
 			#scriptLines += ("./fit.py -i "+subDirectory+fileName+".root" +" -s "+fileName+" -y "+baseDir+"/config/"+aFile+" -f "+aModel+" -o "+subDirectory+fileName+"_eff.root;\n")
-			scriptFile.write(scriptLines)
-			scriptFile.close()
-			jobsFiles = open(jobsDirectory+"/jobs/sendJobs.cmd","a")
-			jobsFiles.write("qsub  "+jobsDirectory+'/jobs/mkhis_'+year+fileName+'.sh\n')
-			jobsFiles.close()
+      scriptFile.write(scriptLines)
+      scriptFile.close()
+      jobsFiles = open(jobsDirectory+"/jobs/sendJobs.cmd","a")
+      jobsFiles.write("qsub  "+jobsDirectory+'/jobs/mkhis_'+year+fileName+'.sh\n')
+      jobsFiles.close()
 
 def main():
     global jobsDirectory
